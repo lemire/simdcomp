c8d9ed7bccb759c0874bfcd6215c08e11f14de1f
Daniel Lemire <lemire@gmail.com>
2014-06-03 09:24:33 -0400

    Saving...

--- include/simdcomputil.h
+++ include/simdcomputil.h
@@ -14,7 +14,7 @@
 // returns the integer logarithm of v (bit width)
 uint32_t bits(const uint32_t v);
 
-// max integer logarithm over a range
+// max integer logarithm over a range of SIMDBlockSize integers (128 integer)
 uint32_t maxbits(const uint32_t * begin);
 
 enum{ SIMDBlockSize = 128};
