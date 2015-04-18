acf73cdb156f70091bf0b6faaaf182549b0c8d47
Daniel Lemire <lemire@gmail.com>
2015-04-18 12:44:18 -0400

    Branchless is much faster...

--- src/benchmark.c
+++ src/benchmark.c
@@ -10,8 +10,8 @@
 
 
 void benchmarkSelect() {
-	uint32_t buffer[128];
-	uint32_t backbuffer[128];
+    uint32_t buffer[128];
+    uint32_t backbuffer[128];
     uint32_t initial = 33;
     uint32_t b;
     clock_t S1, S2, S3;
@@ -63,4 +63,5 @@ void benchmarkSelect() {
 }
 int main() {
 	benchmarkSelect();
+        return 0;
 }
