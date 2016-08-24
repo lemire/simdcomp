442a54f76f96adf8a3cdc6e9ea9a400f638bd8a6
Daniel Lemire <lemire@gmail.com>
2016-08-24 15:40:04 -0400

    Switching instruction

--- src/avx512bitpacking.c
+++ src/avx512bitpacking.c
@@ -12,7 +12,7 @@ static uint32_t maxbitas32int(const __m256i accumulator) {
 
 static uint32_t avx512maxbitas32int(const __m512i accumulator) {
   uint32_t ans1 = maxbitas32int(_mm512_castsi512_si256(accumulator));
-  uint32_t ans2 = maxbitas32int(_mm512_extracti32x8_epi32(accumulator,1));
+  uint32_t ans2 = maxbitas32int(_mm512_extracti64x4_epi64(accumulator,1));
 	uint32_t ans = ans1 > ans2 ? ans1 : ans2;
 	return bits(ans);
 }
